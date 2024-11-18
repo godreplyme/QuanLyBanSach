3e94a297f50612213156c9a4066acefad53fd4ad
godreplyme <nguyenhovu0984@gmail.com>
2024-11-18 22:53:11 +0700

    update ui

--- app/index.py
+++ app/index.py
@@ -1,9 +1,11 @@
 from app import app
+from flask import render_template
+
 
 
 @app.route("/")
 def index():
-    print('hello')
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
